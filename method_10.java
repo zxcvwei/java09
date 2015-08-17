cab81bf5eb5c0bbbf45cf150c7939ee29c6e4330
zxcvwei <104021031@live.asia.edu.tw>
2015-08-17 15:43:25 +0800

    java10
    method

--- method_10.java
+++ method_10.java
@@ -4,21 +4,19 @@ public class method_10 {
 		// TODO Auto-generated method stub
 		Scanner scn = new Scanner(System.in);
 		System.out.println("½Ð¿é¤J¤@¦r¦ê");
-		String a=scn.nextLine();
+		String a = scn.nextLine();
 		System.out.print(fun1(a));
 	}
-	public static int fun1 (String xx){
-		char b[]=xx.toCharArray();
+	public static int fun1(String xx) {
+		char b[] = xx.toCharArray();
 		int sum = 0;
-		int aa[]=new int [b.length];
-		for(int i=0;i<b.length;i++){
-			if(b[i]<='9'&&b[i]>='0'){
-				aa[i]=b[i]-'0';
-				sum= sum+aa[i];
+		int aa[] = new int[b.length];
+		for (int i = 0; i < b.length; i++) {
+			if (b[i] <= '9' && b[i] >= '0') {
+				aa[i] = b[i] - '0';
+				sum = sum + aa[i];
 			}
-			
 		}
 		return sum;
 	}
-
 }
