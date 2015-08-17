2e79612a262f85e06926ca52a35ef219a12ddb87
zxcvwei <104021031@live.asia.edu.tw>
2015-08-17 13:54:15 +0800

    java
    method

--- method_07.java
+++ method_07.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class method_06 {
+public class method_07 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scn = new Scanner(System.in);
@@ -7,13 +7,16 @@ public class method_06 {
 		int n = scn.nextInt();
 		System.out.print(fun1(n));
 	}
-	public static int fun1(int n) {
+	public static double fun1(int n) {
 		int x = 1;
-		int sum = 0;
+		double sum = 0;
 		while (x <= n) {
-			sum = sum + x * (x + 1);
+			sum = sum + (1.0/*轉成小數運算*/ / ((2 * x - 1) * (2 * x)));
 			x++;
 		}
+		/*for(int i=1;i<=n;i++){
+			sum = sum + (1-->整數運算 / ((2 * i - 1) * (2 * i)));
+		}*/
 		return sum;
 	}
 }
